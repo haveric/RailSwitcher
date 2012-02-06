a6f7eb720ccac87e6e2e7b7c41886c34550e83ba
Ryan <breuerryan@gmail.com>
2012-02-06 13:58:56 -0600

    Reorganizing

--- src/haveric/railSwitcher/RSPlayerInteract.java
+++ src/haveric/railSwitcher/RSPlayerInteract.java
@@ -18,94 +18,119 @@ import org.bukkit.inventory.PlayerInventory;
 public class RSPlayerInteract implements Listener{
 
 	public static RailSwitcher plugin;
-	
+
+	private ItemStack holding;
+	private PlayerInventory inventory;
+
 	public RSPlayerInteract(RailSwitcher rs) {
 		plugin = rs;
 	}
-	
+
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event) {
-		Permission perm = plugin.getPerm();
-		Player player = event.getPlayer();
 
-		Block block = event.getClickedBlock();
-		
-		
-		ItemStack holding = player.getItemInHand();
-		
-		World world = player.getWorld();
-		
-		PlayerInventory inventory = player.getInventory();
-		
-			Material hand = holding.getType();
-			if (event.getAction() == Action.RIGHT_CLICK_BLOCK && 
-			   (hand == Material.SHEARS || hand == Material.RAILS || hand == Material.POWERED_RAIL || hand == Material.DETECTOR_RAIL)) {
-				if (perm == null || perm.has(player,  "railswitcher.switch")) {
-				
+		Player player = event.getPlayer();
+		holding = player.getItemInHand();
+		Material hand = holding.getType();
+
+		if (event.getAction() == Action.RIGHT_CLICK_BLOCK &&
+		   (hand == Material.SHEARS || hand == Material.RAILS || hand == Material.POWERED_RAIL || hand == Material.DETECTOR_RAIL)) {
+
+			Permission perm = plugin.getPerm();
+			if (perm == null || perm.has(player,  "railswitcher.switch")) {
+
+				World world = player.getWorld();
+				Block block = event.getClickedBlock();
 				Material type = block.getType();
+				inventory = player.getInventory();
+
 				int data = block.getData();
-	
+
 				int bx = block.getX();
 				int by = block.getY();
 				int bz = block.getZ();
-				if (type == Material.RAILS && (hand == Material.SHEARS || hand == Material.RAILS)) {
-					if (data < 9) {
-						if (data == 1 && !canPlaceRail(player,world.getBlockAt(bx+1,by,bz).getType())) {
-							data++;
-						}
-						if (data == 2 && !canPlaceRail(player,world.getBlockAt(bx-1,by,bz).getType())) {
-							data++;
-						}
-						if (data == 3 && !canPlaceRail(player,world.getBlockAt(bx,by,bz-1).getType())) {
-							data++;
-						}
-						if (data == 4 && !canPlaceRail(player,world.getBlockAt(bx,by,bz+1).getType())) {
-							data++;
-						}
-						
-						block.setData((byte)(data+1));
-	
-					} else {
-						block.setData((byte)0);
-					}
-				} else if (type == Material.POWERED_RAIL && (hand == Material.SHEARS || hand == Material.POWERED_RAIL)) {
-					if (data == 5) {
-						block.setData((byte)0);
-					} else if (data == 13) {
-						block.setData((byte)8);
-					} else {
-						if ((data == 1 || data == 9) && !canPlaceRail(player,world.getBlockAt(bx+1,by,bz).getType())) {
-							data++;
-						}
-						if ((data == 2 || data == 10) && !canPlaceRail(player,world.getBlockAt(bx-1,by,bz).getType())) {
-							data++;
-						}
-						if ((data == 3 || data == 11) && !canPlaceRail(player,world.getBlockAt(bx,by,bz-1).getType())) {
-							data++;
-						}
-						if ((data == 4 || data == 12) && !canPlaceRail(player,world.getBlockAt(bx,by,bz+1).getType())) {
-							data++;
-						}
-						if (data == 5) {
+				if (type == Material.RAILS) {
+					if (hand == Material.SHEARS || hand == Material.RAILS) {
+						if (data == 9) {
 							block.setData((byte)0);
 						} else {
+							if (data == 1 && !canPlaceRail(world.getBlockAt(bx+1,by,bz).getType())) {
+								data++;
+							}
+							if (data == 2 && !canPlaceRail(world.getBlockAt(bx-1,by,bz).getType())) {
+								data++;
+							}
+							if (data == 3 && !canPlaceRail(world.getBlockAt(bx,by,bz-1).getType())) {
+								data++;
+							}
+							if (data == 4 && !canPlaceRail(world.getBlockAt(bx,by,bz+1).getType())) {
+								data++;
+							}
+
+							block.setData((byte) (data+1));
+						}
+					} else {
+						block.breakNaturally();
+						if (hand == Material.POWERED_RAIL) {
+							block.setType(Material.POWERED_RAIL);
+						} else if (hand == Material.DETECTOR_RAIL) {
+							block.setType(Material.DETECTOR_RAIL);
+						}
+						if (data > 5){
+							data = 0;
+						}
+						block.setData((byte) data);
+
+						useItemInHand();
+					}
+				} else if (type == Material.POWERED_RAIL) {
+					if (hand == Material.SHEARS || hand == Material.POWERED_RAIL) {
+						if (data == 5) {
+							block.setData((byte)0);
+						} else if (data == 13) {
+							block.setData((byte)8);
+						} else {
+							if ((data == 1 || data == 9) && !canPlaceRail(world.getBlockAt(bx+1,by,bz).getType())) {
+								data++;
+							}
+							if ((data == 2 || data == 10) && !canPlaceRail(world.getBlockAt(bx-1,by,bz).getType())) {
+								data++;
+							}
+							if ((data == 3 || data == 11) && !canPlaceRail(world.getBlockAt(bx,by,bz-1).getType())) {
+								data++;
+							}
+							if ((data == 4 || data == 12) && !canPlaceRail(world.getBlockAt(bx,by,bz+1).getType())) {
+								block.setData((byte)0);
+								return;
+							}
 							block.setData((byte)(data+1));
 						}
+					} else {
+						block.breakNaturally();
+						if (hand == Material.RAILS) {
+							block.setType(Material.RAILS);
+						} else if (hand == Material.DETECTOR_RAIL) {
+							block.setType(Material.DETECTOR_RAIL);
+						}
+						if (data > 5){
+							data = 0;
+						}
+						block.setData((byte) data);
 
+						useItemInHand();
 					}
-				} else if (type == Material.DETECTOR_RAIL && (hand == Material.SHEARS || hand == Material.DETECTOR_RAIL)) {
-					if (data < 5) {
-						
-						if (data == 1 && !canPlaceRail(player,world.getBlockAt(bx+1,by,bz).getType())) {
+				} else if (type == Material.DETECTOR_RAIL) {
+					if (hand == Material.SHEARS || hand == Material.DETECTOR_RAIL) {
+						if (data == 1 && !canPlaceRail(world.getBlockAt(bx+1,by,bz).getType())) {
 							data++;
 						}
-						if (data == 2 && !canPlaceRail(player,world.getBlockAt(bx-1,by,bz).getType())) {
+						if (data == 2 && !canPlaceRail(world.getBlockAt(bx-1,by,bz).getType())) {
 							data++;
 						}
-						if (data == 3 && !canPlaceRail(player,world.getBlockAt(bx,by,bz-1).getType())) {
+						if (data == 3 && !canPlaceRail(world.getBlockAt(bx,by,bz-1).getType())) {
 							data++;
 						}
-						if (data == 4 && !canPlaceRail(player,world.getBlockAt(bx,by,bz+1).getType())) {
+						if (data == 4 && !canPlaceRail(world.getBlockAt(bx,by,bz+1).getType())) {
 							data++;
 						}
 						if (data == 5){
@@ -113,72 +138,26 @@ public class RSPlayerInteract implements Listener{
 						} else {
 							block.setData((byte)(data+1));
 						}
-						
 					} else {
-						block.setData((byte)0);
+						block.breakNaturally();
+						if (hand == Material.POWERED_RAIL) {
+							block.setType(Material.POWERED_RAIL);
+						} else if (hand == Material.RAILS) {
+							block.setType(Material.RAILS);
+						}
+						if (data > 5){
+							data = 0;
+						}
+						block.setData((byte) data);
+
+						useItemInHand();
 					}
-				} else if (type == Material.RAILS) {
-					block.breakNaturally();
-					if (hand == Material.POWERED_RAIL) {
-						block.setType(Material.POWERED_RAIL);	                    
-					} else if (hand == Material.DETECTOR_RAIL) {
-						block.setType(Material.DETECTOR_RAIL);						
-					}
-					if (data > 5){
-						data = 0;
-					}
-					block.setData((byte) data);
-					
-                    int amt = holding.getAmount();
-                    if (amt > 1){
-                        holding.setAmount(--amt);
-                    } else {
-                    	inventory.setItemInHand(null);                        
-                    }
-				} else if (type == Material.POWERED_RAIL) {
-					block.breakNaturally();
-					if (hand == Material.RAILS) {
-						block.setType(Material.RAILS);
-					} else if (hand == Material.DETECTOR_RAIL) {
-						block.setType(Material.DETECTOR_RAIL);
-					}
-					if (data > 5){
-						data = 0;
-					}
-					block.setData((byte) data);
-					
-                    int amt = holding.getAmount();
-                    if (amt > 1){
-                        holding.setAmount(--amt);
-                    } else {
-                    	inventory.setItemInHand(null);
-                        
-                    }
-				} else if (type == Material.DETECTOR_RAIL) {
-					block.breakNaturally();
-					if (hand == Material.POWERED_RAIL) {
-						block.setType(Material.POWERED_RAIL);
-					} else if (hand == Material.RAILS) {
-						block.setType(Material.RAILS);
-					}
-					if (data > 5){
-						data = 0;
-					}
-					block.setData((byte) data);
-					
-                    int amt = holding.getAmount();
-                    if (amt > 1){
-                        holding.setAmount(--amt);
-                    } else {
-                    	inventory.setItemInHand(null);
-                        
-                    }
 				}
-			}
-		} 
+			} // end perm check
+		}
 	}
-	
-	private boolean canPlaceRail(Player player,Material m) {
+
+	private boolean canPlaceRail(Material m) {
 		boolean canPlace = true;
 		ArrayList<Material> materials = plugin.getMaterials();
 		if (materials != null) {
@@ -188,7 +167,16 @@ public class RSPlayerInteract implements Listener{
 				 }
 			}
 		}
-		
+
 		return canPlace;
+	}
+
+	private void useItemInHand(){
+        int amt = holding.getAmount();
+        if (amt > 1){
+            holding.setAmount(--amt);
+        } else {
+        	inventory.setItemInHand(null);
+        }
 	}
 }
